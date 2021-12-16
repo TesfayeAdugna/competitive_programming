622828caad21020ac49c5013ac47a86ab56eb906
TesfayeAdugna <adugnatesfaye888@gmail.com>
2021-12-16 18:27:50 +0300

    counting sort problem

---
+++ Week1/counting sort.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    newarray = []
+    for i in range(100):
+        newarray.append(0)
+    for j in arr:
+        newarray[j]+=1
+    return newarray
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
