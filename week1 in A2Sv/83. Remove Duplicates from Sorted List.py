5b46b976992c39c9083103e73a4270c0b3e4f3f1
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-02-17 23:05:41 +0300

    Remove Duplicates from Sorted List

---
+++ week1 in A2Sv/83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,42 @@
+"""
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+Example 1:
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+ 
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+        
+        tempo = head
+        while head.next is not None:
+            if head.val == head.next.val:
+                head.next = head.next.next
+            else:
+                head = head.next
+            
+        head = tempo                
+        return head
