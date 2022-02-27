e74beca58d2eca2c9b6b02de5ae9015d171f3fd7
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-02-27 16:44:31 +0300

    medium

---
+++ a2sv camp/275. H-Index II.py
@@ -0,0 +1,52 @@
+"""
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper and citations is sorted in an ascending order, return compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: A scientist has an index h if h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
+
+If there are several possible values for h, the maximum one is taken as the h-index.
+
+You must write an algorithm that runs in logarithmic time.
+
+Example 1:
+
+Input: citations = [0,1,3,5,6]
+Output: 3
+Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had received 0, 1, 3, 5, 6 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,2,100]
+Output: 2
+
+Constraints:
+
+n == citations.length
+1 <= n <= 105
+0 <= citations[i] <= 1000
+citations is sorted in ascending order.
+"""
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        left = 0
+        n = len(citations)
+        if citations[n-1] == 0:
+            return 0
+        right = n-1
+        mid = (left+right)//2
+        index = n-mid
+        while left<=right:
+            
+            if left == right == mid:
+                return index
+            if citations[mid] >= index:
+                right = mid
+            else:
+                left = mid+1
+            
+            mid = (left+right)//2
+            index = n-mid
