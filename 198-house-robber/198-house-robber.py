7faf4cf856cae2a62c332667e7fd0bc5432c77e1
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-08-11 11:26:31 +0300

    Time: 38 ms (82.24%), Space: 14.1 MB (19.58%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -9,14 +9,16 @@ class Solution:
         
         
         if len(nums) == 1: return nums[0]
-        @lru_cache
+        dic = {}
         def robber(index):
+            if index in dic:
+                return dic[index]
             if index >= len(nums)-2:
                 return nums[index]
             
             left = nums[index] + robber(index+2)
             right = nums[index] + robber(index+3) if index < len(nums)-3 else 0
-            
-            return max(left, right)
+            dic[index] = max(left, right)
+            return dic[index]
         
         return max(robber(0), robber(1))
