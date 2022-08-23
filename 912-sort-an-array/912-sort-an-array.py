9c7f8af7a89f0be268e9484483f2599b2802f088
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-08-23 11:53:26 +0300

    Time: 2556 ms (5.00%), Space: 22.7 MB (28.45%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.py
@@ -0,0 +1,45 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        
+        
+        # using merge sort - divide and conquer
+        def merge(left_arr, right_arr):
+            if not left_arr:
+                return right_arr
+
+            if not right_arr:
+                return left_arr
+
+            result = []
+            left_pointer, right_pointer = 0, 0
+            while len(result) < len(left_arr) + len(right_arr):
+                if left_arr[left_pointer] <= right_arr[right_pointer]:
+                    result.append(left_arr[left_pointer])
+                    left_pointer += 1
+                else:
+                    result.append(right_arr[right_pointer])
+                    right_pointer += 1
+
+                if right_pointer == len(right_arr):
+                    result += left_arr[left_pointer:]
+                    break
+
+                if left_pointer == len(left_arr):
+                    result += right_arr[right_pointer:]
+                    break
+            return result
+        
+        
+        
+        if len(nums) <= 1:
+            return nums
+        
+        mid = len(nums)//2
+        return merge(left_arr=self.sortArray(nums[:mid]), right_arr=self.sortArray(nums[mid:]))
+    
+    
+    
+    
+    
+    
