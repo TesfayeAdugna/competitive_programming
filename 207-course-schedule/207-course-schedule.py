ee281de886bc0d70703665d39ed772a10c51d9a5
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-08-23 09:27:50 +0300

    Time: 170 ms (41.85%), Space: 15.4 MB (97.02%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # build the graph using dictionary.
+        visited = [0] * numCourses
+        graph = defaultdict(list)
+        for courses in prerequisites:
+            graph[courses[1]].append(courses[0])
+            visited[courses[0]] += 1
+            
+        # find a node with no incoming edge and add it to queue.
+        queue = deque()
+        for i in range(numCourses):
+            if visited[i] == 0:
+                queue.append(i)
+                
+        # topological sort algorithm bfs approach.
+        topological_sequence = []
+        total_courses = 0
+        while queue:
+            course = queue.popleft()
+            total_courses += 1
+            topological_sequence.append(course)
+            for prerequisite in graph[course]:
+                visited[prerequisite] -= 1
+                if visited[prerequisite] == 0:
+                    queue.append(prerequisite)
+                    
+        return total_courses == numCourses
+    
+    
+    
+    
+    
