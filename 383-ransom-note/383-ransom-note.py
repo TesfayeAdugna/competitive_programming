d3340ed9250d4b6989b9cb83371e4af5db3fa3a4
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-08-25 20:14:27 +0300

    Time: 62 ms (85.82%), Space: 14.2 MB (53.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -3,10 +3,19 @@ class Solution:
         
         
         
-        ransom = Counter(ransomNote)
-        magazin = Counter(magazine)
+#         ransom = Counter(ransomNote)
+#         magazin = Counter(magazine)
         
-        for key in ransom:
-            if key not in magazin or magazin[key] < ransom[key]:
+#         for key in ransom:
+#             if key not in magazin or magazin[key] < ransom[key]:
+#                 return False
+#         return True
+    
+    
+        magazineL = Counter(magazine)
+        for c in ransomNote:
+            if c in magazineL and magazineL[c] > 0:
+                magazineL[c] -= 1
+            else:
                 return False
         return True
