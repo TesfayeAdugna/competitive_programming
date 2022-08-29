d561fe845c4b00a1d6be6b3a5a085ab0938b0784
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-08-29 15:07:46 +0300

    Time: 253 ms (45.87%), Space: 15.5 MB (11.02%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,21 +1,45 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         
-        left, pivot, right = 0, 0, sum(nums)-nums[0]
-        while pivot < len(nums)-1 and right != left:
-            print("left: ", left, "right: ", right)
-            pivot += 1
-            right -= nums[pivot]
-            left += nums[pivot-1]
+        
+        prefixsum = []
+        suffixsum = []
+        for i in range(len(nums)):
+            if not prefixsum:
+                prefixsum.append(nums[i])
+            else:
+                prefixsum.append(prefixsum[-1] + nums[i])
             
-        return pivot if left == right else -1
+            if not suffixsum:
+                suffixsum.append(nums[-i-1])
+            else:
+                suffixsum.append(suffixsum[-1] + nums[-i-1])
+                
+        suffixsum = suffixsum[::-1]
+        
+        for i in range(len(nums)):
+            if suffixsum[i] == prefixsum[i]:
+                return i
+        return -1
+        
+#         left, pivot, right = 0, 0, sum(nums)-nums[0]
+#         while pivot < len(nums)-1 and right != left:
+#             pivot += 1
+#             right -= nums[pivot]
+#             left += nums[pivot-1]
+            
+#         return pivot if left == right else -1
+        
+    
     
     """
-    1, 7, 3, 6, 5, 6
+    1, 8, 11, 17, 22, 6
+    28, 27, 20, 17, 11, 6
     
-    1, 8, 11, 17, 22, 28
+    1, 3, 6
+    6, 5, 3
     
-    left = 0
-    pivot = 0
+    2, 3, 2
+    2, 0, -1
     
     """
