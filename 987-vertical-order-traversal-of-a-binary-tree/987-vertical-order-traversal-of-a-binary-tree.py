0c1e0dd33ce8a723b2ce583a7d084d830a9b278c
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-04 21:30:42 +0300

    Time: 45 ms (72.60%), Space: 14.2 MB (72.74%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -7,45 +7,31 @@
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        vertical = defaultdict(list)
-        # traverse the tree using dfs (recursive function.)
+        collect_col_row_val = []
         def dfs(root, row, col):
             if not root:
                 return
             
-            vertical[(row, col)].append(root.val)
+            collect_col_row_val.append([col, row, root.val])
             
             dfs(root.left, row+1, col-1)
             dfs(root.right, row+1, col+1)
             return
         
-        # call the function
         dfs(root, 0, 0)
         
-        # sort the values on the same row and column
-        toSortVertical = vertical.items()
-        listVertical = list(toSortVertical)
-        listVertical.sort()
-        
-        vertical_col = defaultdict(list)
-        for rows in listVertical:
-            index = rows[0]
-            values = rows[1]
-            values.sort()
-            vertical_col[index[1]].extend(values)
-                    
-        # sort the column and collect the answer.
-        toSort = vertical_col.items()
-        listToSort = list(toSort)
-        listToSort.sort()
-        
+        collect_col_row_val.sort()
         Answer = []
-        for tuples in listToSort:
-            temp = tuples[1]
+        i = 0
+        while i < len(collect_col_row_val):
+            temp = []
+            temp.append(collect_col_row_val[i][2])
+            prev = collect_col_row_val[i][0]
+            i += 1
+            while i < len(collect_col_row_val) and collect_col_row_val[i][0] == prev:
+                temp.append(collect_col_row_val[i][2])
+                i += 1
+                
             Answer.append(temp)
+                
         return Answer
-    
-    
-    
-    
-    
