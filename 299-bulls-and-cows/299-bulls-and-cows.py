545aedefb47eaa6ecaac161bfc46c10339e231b4
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-12 21:59:50 +0300

    Time: 113 ms (7.15%), Space: 13.9 MB (80.03%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        
+        
+        
+        
+        c = Counter(secret)
+        bull = 0
+        cow = 0
+        for i in range(len(guess)):
+            if guess[i] == secret[i]:
+                bull += 1
+                if c[guess[i]] > 0:
+                    c[guess[i]]-=1
+                else:
+                    cow -= 1
+            elif guess[i] in c and c[guess[i]]>0:
+                c[guess[i]] -= 1
+                cow += 1
+                    
+        return str(bull) + "A" + str(cow) + "B"
