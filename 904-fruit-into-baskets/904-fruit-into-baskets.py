17e4a0f39717d0260d351900995f8af6f09c2ac5
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-14 16:13:52 +0300

    Time: 2787 ms (5.04%), Space: 20.3 MB (33.78%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,18 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        left, right, maxFruit = 0, 0, 0
-        fruitType = {}
+        
+        
+        
+        
+        left, right, maxFruit, fruitType = 0, 0, 0, defaultdict(int)
         while right < len(fruits):
             if len(fruitType) <= 1 or fruits[right] in fruitType:
-                if fruits[right] in fruitType:
-                    fruitType[fruits[right]] += 1
-                else:
-                    fruitType[fruits[right]] = 1
-                right += 1
+                fruitType[fruits[right]], right = fruitType[fruits[right]]+1, right+1
             else:
                 fruitType[fruits[left]] -= 1
-                if fruitType[fruits[left]] == 0:
-                    del fruitType[fruits[left]]
+                if fruitType[fruits[left]] == 0: del fruitType[fruits[left]]
                 left += 1
             maxFruit = max(maxFruit, right-left)
         return maxFruit
