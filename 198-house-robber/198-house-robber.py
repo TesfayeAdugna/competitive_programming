9442a18bba77e676b9e102a230f613e79ffb9ee9
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-15 18:08:48 +0300

    Time: 34 ms (91.42%), Space: 13.9 MB (65.27%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        
-        
-        
-        
-        
-        
-        
         if len(nums) == 1: return nums[0]
         dic = {}
         def robber(index):
