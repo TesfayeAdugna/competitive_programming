fda21a5c192a81fbc3036e73452b7d8d0b41dbc8
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-18 07:00:37 +0300

    Time: 1518 ms (96.03%), Space: 26.4 MB (66.87%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.py
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -2,15 +2,15 @@ class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
         
         ans=[]
-        dict= {word[::-1]: i for i, word in enumerate(words)}
+        dic = {word[::-1]: i for i, word in enumerate(words)}
         for i, word in enumerate(words):
-            if "" in dict and dict[""] != i and word== word[::-1]:
-                ans.append([i, dict[""]])
+            if "" in dic and dic[""] != i and word== word[::-1]:
+                ans.append([i, dic[""]])
             for j in range(1, len(word)+1):
                 left= word[:j]
                 right= word[j:]
-                if left in dict and dict[left]!= i and right== right[::-1]:
-                    ans.append([i, dict[left]])
-                if right in dict and dict[right]!= i and left== left[::-1]:
-                    ans.append([dict[right], i])
+                if left in dic and dic[left] != i and right == right[::-1]:
+                    ans.append([i, dic[left]])
+                if right in dic and dic[right] != i and left == left[::-1]:
+                    ans.append([dic[right], i])
         return ans
