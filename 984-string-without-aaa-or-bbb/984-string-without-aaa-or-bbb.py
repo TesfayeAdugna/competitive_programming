fa4634fc223af6fab2b318234155445f7728398d
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-19 21:32:45 +0300

    Time: 66 ms (13.72%), Space: 14 MB (25.33%) - LeetHub

---
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.py
@@ -0,0 +1,52 @@
+class Solution:
+    def strWithout3a3b(self, a: int, b: int) -> str:
+        
+        
+        """
+        a = 4, b = 3
+        ababa
+        aababab
+        
+        a = 4, b = 1
+        aabaa
+        
+        a = 4, b = 2
+        abaaba
+        """
+        ans = []
+        flag = a > b
+        while a > 0 or b > 0:
+            if flag:
+                if a > b and a >= 2:
+                    ans.append("aa")
+                    a -= 2
+                else:
+                    ans.append("a")
+                    a -= 1
+                flag = not flag
+            else:
+                if b > a and b >= 2:
+                    ans.append("bb")
+                    b -= 2
+                else:
+                    ans.append("b")
+                    b -= 1
+                flag = not flag
+                    
+        return "".join(ans)
+                    
+                    
+        
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
