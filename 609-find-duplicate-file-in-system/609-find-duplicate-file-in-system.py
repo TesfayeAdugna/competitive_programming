42ac6cae17e96afdb1cbc4941bd3715c68fe53ac
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-19 18:16:29 +0300

    Time: 312 ms (5.08%), Space: 23.9 MB (67.80%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -5,12 +5,10 @@ class Solution:
             temp = path.split()
             for i in range(1, len(temp)):
                 file = temp[i].split(".")
-                file_path = file[0]
-                file_name = file[1]
+                file_path, file_name = file[0], file[1]
                 file_content = file_name[4:-1]
                 dic[file_content].append(temp[0]+"/"+file_path+"."+file_name[:3])
         answer = []
         for key in dic:
-            if len(dic[key]) > 1:
-                answer.append(dic[key])
+            if len(dic[key]) > 1: answer.append(dic[key])
         return answer
