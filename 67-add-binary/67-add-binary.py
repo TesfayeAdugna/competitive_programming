c225e366d7dca33d3853a407410a500c8eac7d5f
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-21 17:26:25 +0300

    Time: 54 ms (54.11%), Space: 13.8 MB (72.62%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,8 +1,4 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         
-        
-        value_a = int(a, 2)
-        value_b = int(b, 2)
-        
-        return bin(value_a + value_b)[2:]
+        return bin(int(a, 2) + int(b, 2))[2:]
