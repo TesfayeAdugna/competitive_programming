cd7982269c2b5231f9fae3591db4feb637ad326e
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-22 11:43:19 +0300

    Time: 40 ms (85.61%), Space: 14.1 MB (35.92%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,11 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        
-        
+
+        print("2&4: ", 2 & 2)
+        n = len(nums)
         answer = []
-        for i in range(len(nums)+1):
-            x = list(combinations(nums, i))
-            answer.extend(x)
+        index = 0
+        while index < (1<<n):
+            subset = []
+            for i in range(32):
+                if index & (1<<i):
+                    subset.append(nums[i])
+            answer.append(subset)
+            index += 1
             
         return answer
