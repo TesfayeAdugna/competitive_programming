b966a3ed64a7024b94d9466e87828ebe22bdfc27
Tesfaye Adugna <91592825+TesfayeAdugna@users.noreply.github.com>
2022-09-22 11:47:29 +0300

    Time: 76 ms (8.96%), Space: 14.2 MB (35.92%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,10 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-
-        n = len(nums)
-        answer = []
-        index = 0
+        answer, index, n = [], 0, len(nums)
         while index < (1<<n):
             subset = []
             for i in range(32):
-                if index & (1<<i):
-                    subset.append(nums[i])
+                if index & (1<<i): subset.append(nums[i])
             answer.append(subset)
             index += 1
-            
         return answer
